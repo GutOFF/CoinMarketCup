83107e8a0907150a0aefbb0a86cb9a62b5eacff0
GutOFF <gyhadyt@mail.ru>
2021-11-30 19:09:23 +0500

    Добавление базового репозитория

--- CoinMarketCup/Repository/RepositoryBase.cs
+++ CoinMarketCup/Repository/RepositoryBase.cs
@@ -2,10 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoinMarketCup.Interface;
+using Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoinMarketCup.Repository
 {
-    public class RepositoryBase
+    public class RepositoryBase<T> : IRepositoryBase<T> 
+        where T : class
     {
+        protected ApplicationDbContext Context;
+
+        public RepositoryBase(ApplicationDbContext context)
+        {
+            Context = context;
+        }
+
+        public async Task Add(T entity)
+        {
+            await Context.Set<T>().AddAsync(entity);
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task Edit(T entity)
+        {
+            Context.Set<T>().Update(entity).State  = EntityState.Modified; ;
+            await Context.SaveChangesAsync();
+        }
+
+        public async ValueTask<T> GetById(string id)
+        {
+            var result= await Context.Set<T>().FindAsync(id);
+            return result;
+        }
+
+        public async Task Remove(T entity)
+        {
+             Context.Set<T>().Remove(entity);
+             await Context.SaveChangesAsync();
+        }
     }
 }
---
+++ CoinMarketCup/Repository/RoleRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entity;
+using Microsoft.AspNetCore.Identity;
+
+namespace CoinMarketCup.Repository
+{
+    public class RoleRepository : RepositoryBase<IdentityRole>
+    {
+        public RoleRepository(ApplicationDbContext context) : base(context)
+        {
+                
+        }
+    }
+}
